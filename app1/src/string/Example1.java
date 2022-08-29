c307c2cee80fc162f93c121b263f6ff4263aeaf6
Yaswanth B <yaswanthbudhavaram@gmail.com>
2022-08-29 23:45:03 +0530

    adding some more files

---
+++ app1/src/string/Example1.java
@@ -0,0 +1,11 @@
+package string;
+
+public class Example1 {
+public static void main(String[] args) {
+	String name="yash+jash+fyasgujhsajks+wsfygujahkjc+gkcs.,lka.cs,.ilaksfhgjhbaxgjchsbjhjdnc"
+				+"hgxasygjhbjc+axygjkhs+sxyjgjkcs+scbjkbknk,c+shiuhfjdghuhjkndfkjkm.bmfglm.glj"
+			+"sdyguihgkj+fdsjkhk,ngfb+jhdcgbjkkbgf";
+			int count=name.length();
+			System.out.println(count);
+}
+}
